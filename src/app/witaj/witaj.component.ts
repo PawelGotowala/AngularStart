b862ff06ce073b92230514d6f48c49a6b10b4dc6
PawelGotowala <43782752+PawelGotowala@users.noreply.github.com>
2019-10-07 14:09:27 +0200

    property binding

--- src/app/witaj/witaj.component.ts
+++ src/app/witaj/witaj.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-witaj',
-  templateUrl: './witaj.component.html',
-  styleUrls: ['./witaj.component.css']
+    selector: 'app-witaj',
+    templateUrl: './witaj.component.html',
+    styleUrls: ['./witaj.component.css']
 })
 export class WitajComponent implements OnInit {
 
-  constructor() { }
+    inputText = 'tekst';
+    maxLarge = 5;
+    colorClass = 'color';
+    logoUrl = '';
+    isDisable = true;
 
-  ngOnInit() {
-  }
+    constructor() {
+        setTimeout(() => {
+            this.isDisable = false;
+        }, 3000);
+    }
+
+    change() {
+        this.inputText = 'zamian tekstu i koloru';
+        this.maxLarge = 10;
+        this.colorClass = 'color2';
+        this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    }
+
+    ngOnInit() {
+    }
 
 }
