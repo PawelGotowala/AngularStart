78271eabb0dd71e5ac368e3c3a92d183971669a9
PawelGotowala <43782752+PawelGotowala@users.noreply.github.com>
2019-10-07 14:28:58 +0200

    event binding

--- src/app/witaj/witaj.component.ts
+++ src/app/witaj/witaj.component.ts
@@ -7,23 +7,38 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WitajComponent implements OnInit {
 
-    inputText = 'tekst';
+    inputText = '';
     maxLarge = 5;
     colorClass = 'color';
     logoUrl = '';
     isDisable = true;
 
-    constructor() {
-        setTimeout(() => {
-            this.isDisable = false;
-        }, 3000);
-    }
 
     change() {
         this.inputText = 'zamian tekstu i koloru';
         this.maxLarge = 10;
         this.colorClass = 'color2';
         this.logoUrl = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    }
+
+    onFocus() {
+        this.colorClass = 'color2';
+    }
+
+    onClick(event) {
+        console.log(event);
+    }
+
+    onMouseMove(event) {
+        console.log('x:' + event.clientX + 'y:' + event.clientY);
+    }
+
+    onPaste() {
+        this.inputText = 'nie wklejaj, wpisz';
+    }
+
+    change2() {
+        this.isDisable = false;
     }
 
     ngOnInit() {
